2becb7650dbdb5194171e3de9ce77894678b1136
fizalangah <fizalangah2001@gmail.com>
2024-10-02 05:06:53 -0700

    some changes

--- app/page.tsx
+++ app/page.tsx
@@ -8,11 +8,11 @@ import { useState, ChangeEvent } from "react";
 // Import custom UI components from the UI directory
 import {
   Card,
-  CardHeader,
-  CardTitle,
-  CardDescription,
-  CardContent,
-  CardFooter,
+  // CardHeader,
+  // CardTitle,
+  // CardDescription,
+  // CardContent,
+  // CardFooter,
 } from "@/components/card";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
